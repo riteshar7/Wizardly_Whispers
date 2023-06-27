72ff5aa0560e3e4225f4ba43bad06f23c47ef329
riteshar7 <ritesh.ranjan@iitg.ac.in>
2023-06-28 02:10:29 +0530

    Added server and client establishing two-way connection using socket.io

---
+++ nodeServer/index.js
@@ -0,0 +1,31 @@
+// This is the Node Server which will handle our Socket IO connections
+// const http = require('http');
+// const server = http.createServer();
+
+const express = require('express');
+const app = express();
+
+const server = app.listen(8000);
+
+const io = require('socket.io')(server,{
+    cors: {
+        origin: '*',
+    }
+});
+
+
+const users = {};
+
+io.on('connection', (socket) => {
+    socket.on('new-user-joined', (name) => {
+        console.log(name,'joined');
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+        console.log(users);
+    });
+    socket.on('send', (message) => {
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]});
+    });
+})
+
+
