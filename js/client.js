d89b31dd2fa29aeb6f1ab9e8b06dc68807915d63
riteshar7 <ritesh.ranjan@iitg.ac.in>
2023-06-28 12:52:02 +0530

    Enabled sending and Receiving messages through socket.io

--- js/client.js
+++ js/client.js
@@ -4,5 +4,29 @@ const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
 const messageContainer = document.querySelector('.container');
 
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message =messageInput.value;
+    append(`Me: ${message}`,'right');
+    socket.emit('send', message);
+    messageInput.value='';
+})
+
 const name_ofuser = prompt('Enter Your Name to join');
 socket.emit('new-user-joined', name_ofuser);
+
+socket.on('user-joined', (name) => {
+    append(`${name} joined the chat`,'right');
+});
+
+socket.on('receive', (data) => {
+    append(`${data.name}: ${data.message}`, 'left');
+})
