a1ebcf0e4310c44f77b43a93662db616b1f03ec6
riteshar7 <ritesh.ranjan@iitg.ac.in>
2023-07-06 12:40:24 +0530

    Modified Validation

--- nodeServer/validation/users/user.schema.js
+++ nodeServer/validation/users/user.schema.js
@@ -3,7 +3,9 @@ const joi = require('@hapi/joi');
 const schema = {
     user: joi.object({
         name: joi.string().min(4).max(30).required(),
-        password: joi.string().pattern(new RegExp("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")).required(),
+        password: joi.string().pattern(new RegExp("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{8,}$")),
+        conf_password: joi.string().pattern(new RegExp("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[A-Za-z0-9]{8,}$"),),
+        house: joi.string(),
     })
 };
 
